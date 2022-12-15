adad8f11c64a93077f923b0ac0cd9d603acb2059
Julia Guedes <guedesarce@gmail.com>
2022-12-15 09:08:43 -0300

    ultimo commit

--- src/app.js
+++ src/app.js
@@ -12,7 +12,11 @@ db.connect();
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res)=> res.status(200).send("Bem vinda!"))
+app.get("/",(req,res)=> res.status(200).send({
+    "Sobre o projeto": "O objetivo do app “computador acessível” é unir três tipos de pessoas: Uma ou mais com conhecimento de montagem de computadores. Dois: pessoas com computadores velhos ou quebrados que queiram doar seus hardwares. E por fim três, pessoas de baixa renda que precisam de um computador.",
+    "Informações adicionais": "Este app não tenta substituir uma ONG gratuita, e possui custos de logística para fazer com que o projeto continue, que devem ser pagas pela pessoa beneficiária.",
+    "Como acessar as rotas": "Todas as informações sobre rotas estão disponíveis no about-me deste projeto: https://github.com/Arceguedes/Computador-Acessivel"
+}))
 
 app.use("/users", userRoutes);
 app.use("/hardware", hardwareRoutes);
